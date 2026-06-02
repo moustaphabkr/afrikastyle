1e4b4b3216fc4d6c22b54fec310f121896794dd4
moustaphabkr <moustaphaboukary12@gmail.com>
2026-06-02 20:14:29 +0200

    Mise en page de l'accueil avec categories et nouveaux produits

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .models import Produit
+from .models import Categorie, Produit
 
 def home(request):
-    # On récupère tous les produits de la base de données
-    products = Product.objects.all()
-    # On les envoie à notre future page HTML
-    return render(request, 'catalogue/index.html', {'products': products})
+    # On récupère uniquement les catégories qui n'ont pas de parent (les catégories racines)
+    categories_principales = Categorie.objects.filter(parent__isnull=True)
+    
+    # Optionnel : On peut aussi récupérer les 4 derniers produits ajoutés pour faire une section "Nouveautés"
+    nouveautes = Produit.objects.order_by('-date_ajout')[:4]
+    
+    context = {
+        'categories': categories_principales,
+        'nouveautes': nouveautes,
+    }
+    return render(request, 'catalogue/index.html', context)
