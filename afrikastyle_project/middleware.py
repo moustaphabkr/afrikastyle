38539481cfda8f402ed6cfabbc276e1185299d21
moustaphabkr <moustaphaboukary12@gmail.com>
2026-06-12 20:47:33 +0200

    Fix middleware import

--- afrikastyle_project/middleware.py
+++ afrikastyle_project/middleware.py
@@ -1,17 +1,13 @@
 import os
-from django.http import HttpResponse
+from django.shortcuts import render 
 
 class MaintenanceMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Vérifie la variable d'environnement
-        # On force la lecture en chaîne de caractères pour éviter les erreurs
-        maintenance = os.environ.get('MAINTENANCE_MODE', 'False')
-        
-        if maintenance == 'True':
-            # Assure-toi que le chemin vers maintenance.html est correct
+        # Vérification du mode maintenance
+        if os.environ.get('MAINTENANCE_MODE') == 'True':
             return render(request, 'maintenance.html')
         
         return self.get_response(request)
