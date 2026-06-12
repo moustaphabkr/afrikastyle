4391dab5ec7995580a205ea534a2b8ed150ae74e
moustaphabkr <moustaphaboukary12@gmail.com>
2026-06-12 11:40:56 +0200

    Fix: Nettoyage de i18n_patterns et correction des URLconfs

--- afrikastyle_project/urls.py
+++ afrikastyle_project/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include # <-- On a ajouté "include" ici
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
+# Routes globales sans traduction
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalogue.urls')), # <-- On connecte les URLs du catalogue
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+# Routes du catalogue avec traduction automatique
+urlpatterns += i18n_patterns(
+    path('', include('catalogue.urls')),
+)
--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,14 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns  # <-- L'import magique pour les langues
+from django.urls import path
+from . import views
 
-# 1. Les routes qui ne doivent PAS changer de langue (l'administration et le système de switch)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),  # Permet au futur bouton FR/EN de fonctionner
+    path('', views.home, name='home'),
+    path('categorie/<slug:slug>/', views.categorie_detail, name='categorie_detail'),
 ]
-
-# 2. Les routes de ton catalogue qui basculeront automatiquement entre /fr/ et /en/
-urlpatterns += i18n_patterns(
-    path('', include('catalogue.urls')),  # Inclut ton fichier proprement sans le modifier
-)
