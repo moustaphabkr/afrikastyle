4d6648ada0b01539d4820e50679ffff520028ff4
moustaphabkr <moustaphaboukary12@gmail.com>
2026-06-12 20:38:06 +0200

    Correction lecture variable maintenance

--- afrikastyle_project/middleware.py
+++ afrikastyle_project/middleware.py
@@ -1,14 +1,17 @@
 import os
-from django.shortcuts import render
+from django.http import HttpResponse
 
 class MaintenanceMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # On vérifie la variable d'environnement (par défaut False)
-        if os.getenv('MAINTENANCE_MODE', 'False') == 'True':
-           # Modifie la ligne 11 dans afrikastyle_project/middleware.py
-            return render(request, 'maintenance.html', status=503)
+        # Vérifie la variable d'environnement
+        # On force la lecture en chaîne de caractères pour éviter les erreurs
+        maintenance = os.environ.get('MAINTENANCE_MODE', 'False')
+        
+        if maintenance == 'True':
+            # Assure-toi que le chemin vers maintenance.html est correct
+            return render(request, 'maintenance.html')
         
         return self.get_response(request)
