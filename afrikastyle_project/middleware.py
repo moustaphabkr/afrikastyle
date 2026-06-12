40fd8c0d9dda49810d0958bc1c5eb718889c13b4
moustaphabkr <moustaphaboukary12@gmail.com>
2026-06-12 19:42:34 +0200

    Fix chemin template maintenance

--- afrikastyle_project/middleware.py
+++ afrikastyle_project/middleware.py
@@ -8,6 +8,7 @@ class MaintenanceMiddleware:
     def __call__(self, request):
         # On vérifie la variable d'environnement (par défaut False)
         if os.getenv('MAINTENANCE_MODE', 'False') == 'True':
-            return render(request, 'maintenance.html', status=503)
+           # Modifie la ligne 11 dans afrikastyle_project/middleware.py
+            return render(request, 'catalogue/maintenance.html', status=503)
         
         return self.get_response(request)
