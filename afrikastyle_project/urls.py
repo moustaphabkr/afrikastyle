939e9180567581e4da4ccf9569e6f2633e0d301c
moustaphabkr <moustaphaboukary12@gmail.com>
2026-06-12 11:52:57 +0200

    Clean: Restauration complète du design original sans le module i18n

--- afrikastyle_project/urls.py
+++ afrikastyle_project/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
-# Routes globales sans traduction
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
+    path('', include('catalogue.urls')), # Le catalogue est à la racine directe du site
 ]
 
-# Routes du catalogue avec traduction automatique
-urlpatterns += i18n_patterns(
-    path('', include('catalogue.urls')),
-)
+# Gestion des images des produits en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
